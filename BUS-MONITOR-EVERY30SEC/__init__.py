e2774b7ba285d4ff4e5ce6dd96e14c130ac8ff93
Lukasz <lukasz.bielak1@gmail.com>
2022-07-17 21:36:29 +0200

    update of main function

--- BUS-MONITOR-EVERY30SEC/__init__.py
+++ BUS-MONITOR-EVERY30SEC/__init__.py
@@ -9,8 +9,6 @@ def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
     producer = KafkaProducer()
     producer.run()
     
